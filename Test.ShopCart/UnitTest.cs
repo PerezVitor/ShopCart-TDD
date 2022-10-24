c38ef430e3d7499924e923087a11f053e97b3584
PerezVitor <vitor.g.perez@hotmail.com>
2022-10-23 21:07:09 -0300

    First test created - Insert itme into shopping cart

---
+++ Test.ShopCart/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Test.ShopCart;
+public class UnitTest
+{
+    [Fact]
+    public void Check_InsertItemOnShopCart()
+    {
+        //AAA
+        //Arrange
+        var item = new Item("Shirt", 29.99, 1);
+        var shopCart = new ShopCart();
+
+        //Act
+        shopCart.InsertItem(item);
+
+        //Assert
+        Assert.Contains(item, shopCart.Itens);
+    }
+}
